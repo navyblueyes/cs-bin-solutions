29d375ffdd7d7e2d5b864bfdd4298569d9c4e19a
MichaelMinkoff <mike.v.minkoff@gmail.com>
2020-08-27 19:03:45 -0700

    answered closure no 8; request approval

--- closures.js
+++ closures.js
@@ -128,3 +128,38 @@ function rollCall(names) {
 // rollCaller() // -> Should log 'Ruth'
 // rollCaller() // -> Should log 'Everyone accounted for'
 
+// Challenge 9
+
+/*
+Create a function   saveOutput   that accepts
+  a function (that will accept one argument),
+  and a string (that will act as a password).
+
+saveOutput will then ...
+  return a function that behaves exactly like the passed-in function,
+  ... except if a password string is passed in as an argument...
+    returned function will return ...
+      an object with all previously passed-in arguments as keys,
+      ... and the corresponding outputs as values.
+*/
+
+// CHALLENGE 8
+function saveOutput(func, magicWord) {
+  const loggedOutput = {};
+  function checkStore(num) {
+    if (num === magicWord) {
+      return loggedOutput;
+    } else {
+      loggedOutput[num] = func(num);
+      return loggedOutput[num];
+    }
+  }
+  return checkStore;
+}
+
+// /*** Uncomment these to check your work! ***/
+// const multiplyBy2 = function(num) { return num * 2; };
+// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+// console.log(multBy2AndLog(2)); // => should log 4
+// console.log(multBy2AndLog(9)); // => should log 18
+// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
